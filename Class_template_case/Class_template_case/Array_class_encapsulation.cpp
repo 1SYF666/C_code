fe7fcb87b3839f5ba08f2cf29df60547634e703b
1SYF666 <2832195814@QQ.COM>
2023-06-29 13:00:12 +0800

    second

--- Class_template_case/Class_template_case/Array_class_encapsulation.cpp
+++ Class_template_case/Class_template_case/Array_class_encapsulation.cpp
@@ -29,14 +29,76 @@ void test01()
 	cout << "arr1的打印输出为：" << endl;
 	printIntArray(arr1);
 
+	cout << "arr1的容量为：" << arr1.getCapacity() << endl;
+	cout << "arr1的大小为：" << arr1.getSize() << endl;
 
+
+	MyArray <int>arr2(arr1);
+	cout << "arr1的打印输出为：" << endl;
+	printIntArray(arr2);
+
+	//尾删
+	arr2.Pop_Back();
+
+	cout << "arr2尾删后：" << endl;
+	cout << "arr2的容量为：" << arr2.getCapacity() << endl;
+	cout << "arr2的大小为：" << arr2.getSize() << endl;
 
 }
 
+//测试自定义数据类型
+class Person
+{
+public:
+	Person(){}
+	Person(string name, int age)
+	{
+		this->m_Name = name;
+		this->m_Age = age;
+	}
+
+	string m_Name;
+	int m_Age;
+};
+
+void printPersonArray(MyArray<Person>& arr)
+{
+	for (int i = 0; i < arr.getSize(); i++)
+	{
+		cout << "姓名：" << arr[i].m_Name << "年龄：" << arr[i].m_Age << endl;
+	}
+
+}
+
+void test02()
+{
+	MyArray<Person>arr(10);
+
+	Person p1("孙悟空",999);
+	Person p2("11",111);
+	Person p3("22",222);
+	Person p4("33",333);
+	Person p5("44",444);
+
+	//将数据插入到数组中
+	arr.Push_Back(p1);
+	arr.Push_Back(p2);
+	arr.Push_Back(p3);
+	arr.Push_Back(p4);
+	arr.Push_Back(p5);
+
+	//打印数组
+	printPersonArray(arr);
+
+	//
+	cout << "arr的容量为：" << arr.getCapacity() << endl;
+	cout << "arr的大小为：" << arr.getSize() << endl;
+}
 
 int main()
 {
-	test01();
+	//test01();
+	test02();
 
 	system("pause");
 
