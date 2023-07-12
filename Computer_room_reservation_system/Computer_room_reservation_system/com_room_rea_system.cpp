4962fef3f521130a78bbef75deb90040088d0edf
1SYF666 <2832195814@QQ.COM>
2023-07-12 23:01:50 +0800

    third

--- Computer_room_reservation_system/Computer_room_reservation_system/com_room_rea_system.cpp
+++ Computer_room_reservation_system/Computer_room_reservation_system/com_room_rea_system.cpp
@@ -2,6 +2,95 @@
 #include<iostream>
 using namespace std;
 
+#include"identity.h"
+#include<fstream>
+#include"student.h"
+//登录功能   参数1  操作文件名  参数2  操作身份类型
+void LoginIn(string fileName, int type)
+{
+	//父类指针，用于指向子类对象
+	Identity* person = NULL;
+
+	//读文件
+	ifstream ifs;
+	ifs.open(fileName, ios::in);
+
+	//判断文件是否存在
+	if (!ifs.is_open())
+	{
+		cout << "文件不存在" << endl;
+		ifs.close();
+		return;
+	}
+
+	//准备接受用户信息
+	int id = 0;
+	string name;
+	string pwd;
+
+	//判断身份，
+	if (type == 1)//学生信息
+	{
+		cout << "请输入你的学号：" << endl;
+		cin >> id;
+	}
+	else if (type == 2)
+	{
+		cout << "请输入您的职工号：" << endl;
+		cin >> id;
+	}
+
+	cout << "请输入用户名：" << endl;
+	cin >> name;
+
+	cout << "请输入密码：" << endl;
+	cin >> pwd;
+
+	if (type == 1)
+	{
+		//学生身份验证
+		int fId; //从文件中读取的id号
+		string fName;//从文件中获取的姓名
+		string fPwd;
+		while (ifs>>fId&&ifs>>fName&&ifs>>fPwd)
+		{
+			cout << fId << endl;
+			cout << fName << endl;
+			cout << fPwd << endl;
+			cout << endl;
+			if (fId == id && fName == name && fPwd == pwd)
+			{
+				cout << "学生验证登录成功！" << endl;
+				system("pause");
+				system("clc");
+				person = new Student(id, name, pwd);
+				//进入学生身份的子菜单
+
+
+				return;
+			}
+		}
+
+	}
+	else if (type == 2)
+	{
+		//教师身份验证
+	}
+	else if (type == 3)
+	{
+		//管理员身份验证
+	}
+
+	cout << "验证登录失败！" << endl;
+	system("pause");
+	system("clc");
+
+
+
+
+}
+
+
 
 int main()
 {
