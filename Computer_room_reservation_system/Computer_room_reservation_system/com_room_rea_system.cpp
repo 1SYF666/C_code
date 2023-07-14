e9ec86fc3b1f0e9a6e1a270ed4d40e9db4965a6c
1SYF666 <2832195814@QQ.COM>
2023-07-14 21:23:02 +0800

    fifth

--- Computer_room_reservation_system/Computer_room_reservation_system/com_room_rea_system.cpp
+++ Computer_room_reservation_system/Computer_room_reservation_system/com_room_rea_system.cpp
@@ -6,11 +6,69 @@ using namespace std;
 #include"student.h"
 #include"globalFile.h"
 #include"teacher.h"
+#include"manager.h"
+
+//进入管理员子菜单界面
+void managerMenu(Identity*& manager)
+{
+	while (true)
+	{
+		//调用管理的子菜单
+		manager->openMenu();
+
+		//将父类指针转为子类指针，调用子类里的其他接口
+		Manager* man = (Manager*)manager;
+
+		int select = 0;
+
+		//接受用户选项
+		cin >> select;
+
+		if (select == 1)//添加账号
+		{
+			cout << "添加账号" << endl;
+			man->addPerson();
+		}
+		else if (select == 2)//查看账号
+		{
+			cout << "查看账号" << endl;
+			man->showPerson();
+		}
+		else if (select == 3)//查看机房
+		{
+			cout << "查看机房" << endl;
+			man->showPerson();
+		}
+		else if (select == 4)//清空预约
+		{
+			cout << "清空预约" << endl;
+			man->cleanFile();
+
+		}
+		else
+		{
+			//注销
+			delete manager;//销毁掉堆区对象
+			cout << "注销成功" << endl;
+			system("pause");
+			system("clc");
+			return;
+
+
+		}
+
+	}
+
+
+}
+
+
+
 //登录功能   参数1  操作文件名  参数2  操作身份类型
 void LoginIn(string fileName, int type)
 {
 	//父类指针，用于指向子类对象
-	Identity* person = NULL;
+	Identity * person = NULL;
 
 	//读文件
 	ifstream ifs;
@@ -59,7 +117,7 @@ void LoginIn(string fileName, int type)
 			//cout << fName << endl;
 			//cout << fPwd << endl;
 			//cout << endl;
-			if (fId == id && fName == name && fPwd == pwd)
+			if ( id == fId  &&  name==fName  &&  pwd==fPwd )
 			{
 				cout << "学生验证登录成功！" << endl;
 				system("pause");
@@ -82,16 +140,14 @@ void LoginIn(string fileName, int type)
 		string fPwd;//从文件中获取的姓名
 		while (ifs >> fId && ifs >> fName && ifs >> fPwd)
 		{
-			//cout << fId << endl;
-			//cout << fName << endl;
-			//cout << fPwd << endl;
-			//cout << endl;
-			if (fId == id && fName == name && fPwd == pwd)
+	
+			if ( id==fId  &&  name==fName  &&  pwd==fPwd )
 			{
 				cout << "教师验证登录成功！" << endl;
 				system("pause");
 				system("clc");
 				person = new Teacher(id, name, pwd);
+
 				//进入教师的子菜单
 
 
@@ -103,12 +159,32 @@ void LoginIn(string fileName, int type)
 	else if (type == 3)
 	{
 		//管理员身份验证
+		string fName; //从文件中获取的姓名
+		string fPwd;//从文件中获取的姓名
+		while (ifs >> fName && ifs >> fPwd)
+		{
+
+			if (name == fName && pwd == fPwd)
+			{
+				cout << "验证登录成功！" << endl;
+				system("pause");
+				system("clc");
+				person = new Manager(name, pwd);
+				//进入教师的子菜单
+				managerMenu(person);
+				
+				
+				
+				return;
+			}
+		}
+
+
 	}
 
 	cout << "验证登录失败！" << endl;
 	system("pause");
 	system("clc");
-
 
 }
 
