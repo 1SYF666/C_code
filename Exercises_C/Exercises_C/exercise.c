00e34fb4380ff6c56619c5bffb9e4e155ea4e13d
1SYF666 <2832195814@QQ.COM>
2023-07-30 21:52:32 +0800

    second

--- Exercises_C/Exercises_C/exercise.c
+++ Exercises_C/Exercises_C/exercise.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include<stdio.h>
+#include"function.h"
 
 //int main()
 //{
@@ -29,20 +30,58 @@
 //}
 
 
+//int main()
+//{
+//	//获得一个年龄
+//	int age;
+//	int tianshu;
+//	printf("请输入年龄：");
+//	scanf("%d" , &age);
+//	//年龄转化天数
+//	tianshu = age * 365;
+//	
+//	//打印输出
+//	printf("年龄为：%d\n", age);
+//	printf("转化天数为：%d\n", tianshu);
+//	
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	jolly();
+//	jolly();
+//	jolly();
+//	deny();
+//	return 0;
+//}
+
+//int main()
+//{
+//	br();
+//	ic();
+//	printf("India,China\n");
+//	printf("Brazil,Russia\n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int toes = 10;
+//	int doubletoes = toes * 2;
+//	int squretoes = toes * toes;
+//	printf("%d 的两倍是：%d，平方是：%d\n", toes, doubletoes, squretoes);
+//
+//	return 0;
+//}
+
 int main()
 {
-	//获得一个年龄
-	int age;
-	int tianshu;
-	printf("请输入年龄：");
-	scanf("%d" , &age);
-	//年龄转化天数
-	tianshu = age * 365;
-	
-	//打印输出
-	printf("年龄为：%d\n", age);
-	printf("转化天数为：%d\n", tianshu);
-	
-
+	Smile();Smile();Smile();
+	printf("\n");
+	Smile(); Smile();
+	printf("\n");
+	Smile();
 	return 0;
 }
