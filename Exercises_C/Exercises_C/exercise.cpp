5327a45015be68a74c2627b33650963d94514aad
1SYF666 <2832195814@QQ.COM>
2023-10-09 23:59:07 +0800

    twelve

--- Exercises_C/Exercises_C/exercise.cpp
+++ Exercises_C/Exercises_C/exercise.cpp
@@ -4605,59 +4605,65 @@
 //*******************2023/10/09 8:57*********************//
 
 //string.c----使用字符串与用户交互
-#define MSG "You must have many talents.Tell me some."    //一个字符串常量
-
-#define LIM 5
-#define LINELEN 81  //最大字符串长度+1
+//#define MSG "You must have many talents.Tell me some."    //一个字符串常量
+//
+//#define LIM 5
+//#define LINELEN 81  //最大字符串长度+1
+//
+//int main()
+//{
+//	char name[LINELEN];
+//	char talents[LINELEN];
+//	int i;
+//	const char m1[40] = "Limits yourself to one line's worth.";   //初始化一个大小已经确定的char 数组
+//	const char m2[] = "If you can't think of anything,fake it.";  //让编译七计算数组的大小
+//	const char* m3 = "\nEnough about me - what's a your name?";
+//	const char* mytal[LIM] = { "Adding numbers swiftly",          //初始化一个字符串指针数组,数组存放的是指针元素，指针指向char类型字符串
+//								"Multiplying accurately","Stashing data",//*mytal[0]=='A', *mytal[1]=='M',
+//								"Following instructions to the letter",
+//								"Understanding the C language" };
+//
+//	printf("Hi! I'm Clyde the computer. I have many talents.\n");
+//	printf("Let me tell you some of them.\n");
+//	puts("what were they? Ah,yes,here's a partial list.");
+//	for (i = 0; i < LIM; i++)
+//	{
+//		//puts(mytal[i]);   //打印计算机功能的列表
+//		puts(*(mytal + i));   //打印计算机功能的列表
+//	}
+//
+//	puts(m3);
+//	gets_s(name);
+//	
+//	printf("Well,%s,%s\n", name, MSG);
+//	printf("%s\n%s\n", m1, m2);
+//
+//	gets_s(talents);
+//
+//	puts("Let's a see if I've got that list:");
+//
+//	puts(talents);
+//
+//	printf("Thanks for the information,%s.\n", name);
+//
+//	return 0;
+//
+//}
 
 int main()
 {
-	char name[LINELEN];
-	char talents[LINELEN];
-	int i;
-	const char m1[40] = "Limits yourself to one line's worth.";  //初始化一个大小已经确定的char 数组
-	const char m2[] = "If you can't think of anything,fake it.";  //让编译七计算数组的大小
-	const char* m3 = "\nEnough about me - what's a your name?";
-	const char* mytal[LIM] = { "Adding numbers swiftly",          //初始化一个字符串指针数组
-								"Multiplying accurately","Stashing data",
-								"Following instructions to the letter",
-								"Understanding the C language" };
+	const char* mesq = "Don't be a fool";
+	const char* copy;
 
-	printf("Hi! I'm Clyde the computer. I have many talents.\n");
-	printf("Let me tell you some of them.\n");
-	puts("what were they? Ah,yes,here's a partial list.");
-	for (i = 0; i < LIM; i++)
-	{
-		puts(mytal[i]);   //打印计算机功能的列表
-	}
+	copy = mesq;
 
-	puts(m3);
-	gets_s(name);
-	
-	printf("Well,%s,%s\n", name, MSG);
-	printf("%s\n%s\n", m1, m2);
+	printf("%s\n", copy);
+	printf("mesq =%s;&mesq=%p;value=%p\n", mesq, &mesq, mesq);
+	printf("copy =%s;&copy=%p;value=%p\n", copy, &copy, copy);
 
-	gets_s(talents);
-
-	puts("Let's a see if I've got that list:");
-
-	puts(talents);
-
-	printf("Thanks for the information,%s.\n", name);
 
 	return 0;
-
-
-
-	
-
-
-
-
-
 }
-
-
 
 
 
